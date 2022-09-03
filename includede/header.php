5d8434721e64c5e88814d3faa3ca3d07792fa7c5
msbala <krish44858@gmail.com>
2022-09-03 14:47:22 +0100

    done

--- includede/header.php
+++ includede/header.php
@@ -11,6 +11,24 @@
     <script src="https://kit.fontawesome.com/16dcf29601.js" crossorigin="anonymous"></script>
     <!-- <link rel="stylesheet" href="style.css"> -->
 <style> 
+/* home flex.. */
+.flexx{
+  display: flex;
+  border:solid 4px blueviolet;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 10px;
+
+}
+.flexx div{
+  width:270px;
+  height:270px;
+  border:double 4px blueviolet;
+  margin:15px;
+}
+/* end */
+
 .navbar{
     background-color: rgb(110, 51, 168) !important;
     
@@ -44,6 +62,12 @@ body{
   display: block;
   width: 400px;
   border-radius:   10px;
+}
+
+@media only screen and (max-width: 500px) {
+  .center form input{
+    width: 270px;
+  }
 }
 .center form label{
   color:white;
@@ -80,6 +104,142 @@ if(isset($title)){
 ?>
 
 
+/* page changing effect .. */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --secondary-color: #151226;
+  --contrast-color: #BF307F;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  z-index: -10;
+  /* background-color: var(--contrast-color); */
+  background-color:red;
+  /* background-color: rgb(220, 215, 230); */
+
+}
+.center{
+  animation: expand .8s ease forwards;
+  background-color: rgb(220, 215, 230);
+  position: relative;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  transition: all .8s ease;
+}
+.flexx {
+  display: flex;
+  height: 100vh;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+  animation: expand .8s ease forwards;
+  background-color: rgb(220, 215, 230);
+  position: relative;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  transition: all .8s ease;
+}
+
+/* .container_content {
+ width: 50%;
+}
+
+.container_content_inner {
+  width: 80%;
+  margin-left: 80px;
+}
+
+.container_outer_img {
+  margin: 50px;
+  width: 50%;
+  overflow: hidden;
+}   
+    
+.container_img {
+  width: 100%;
+  animation: slideIn 1.5s ease-in-out forwards;
+}
+
+.par {
+  height: auto;
+  overflow: hidden;
+}
+
+p{
+  line-height: 28px;
+  transform: translateY(300px);
+  animation: slideUp .8s ease-in-out forwards .8s;
+}
+
+.btns {
+  height: 100%;
+  position: relative;
+  width: 150px;
+  overflow: hidden;
+}
+
+.btns_more {
+  background: transparent;
+  border: 1px solid var(--contrast-color);
+  border-radius: 50px;
+  padding: 8px 12px;
+  color: #BF307F;
+  font-size: 16px;
+  text-transform: uppercase;
+  position: relative;
+  margin-top: 15px;
+  outline: none;
+  transform: translateY(50px);
+  animation: slideUp .8s ease-in-out  forwards 1s;
+}
+
+.title {
+  overflow: hidden;
+  height: auto;
+}
+
+h1 {
+    font-size: 40px;
+    color: var(--contrast-color);
+    margin-bottom: 20px;
+    transform: translateY(100px);
+    animation: slideUp .8s ease forwards .5s;
+} */
+
+@keyframes slideIn {
+  0% {
+    transform: translateX(500px) scale(.2);
+  }
+  100% {
+    transform: translateX(0px) scale(1);
+  }
+}
+
+@keyframes slideUp {
+  0% {
+    transform: translateY(300px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes expand {
+  0% {
+    transform: translateX(1400px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
+}
 
 
 
--- index.php
+++ index.php
@@ -3,7 +3,14 @@ $title="Home";
 // include the header file 
 include_once "includede/header.php";
 ?>
-
+<br>
+<div class="flexx">
+    <div> content</div>
+    <div> content</div>
+    <div> content</div>
+    <div> content</div>
+    <div> content</div>
+</div>
 
 <?php
 // include the footer file ..
